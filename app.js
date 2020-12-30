f19a682e59e259e9973c4e179ae75db3a4016e97
OgulcanCulfa <ogulcanculfa@gmail.com>
2020-12-30 18:44:05 +0300

    changes

--- app.js
+++ app.js
@@ -68,6 +68,6 @@ app.use('/users', users);
 
 // Server setups
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
-app.listen(port, console.log(`Server started on port ${port}`));
+app.listen(PORT,() => {console.log(`Server is listening on PORT: ${PORT}`)});
