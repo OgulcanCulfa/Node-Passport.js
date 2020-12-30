ebc9019a036b6422a21987468938e7172c027392
OgulcanCulfa <ogulcanculfa@gmail.com>
2020-12-30 17:54:29 +0300

    first heroku deploy

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-const connection = mongoose.connect('',{
-    dbName: "",
+const connection = mongoose.connect('mongodb+srv://Ogulcan:fahrireis46@example.i3mn4.mongodb.net/test',{
+    dbName: "passport",
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 .then(() => console.log("MongoDB is on..."))
-.catch(err => console.log(err))
+.catch(err => console.log(err));
 
 module.exports = connection;
